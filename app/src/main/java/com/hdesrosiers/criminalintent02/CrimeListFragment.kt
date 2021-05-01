8805676262b8eb0f3ee3dd4ffa98ce9d9f91a917
rvd2pwipwip <herve_desrosiers@hotmail.com>
2021-05-01 13:59:27 -0400

    Chapter 11 Databases and Room implemented LiveData to use background thread

--- app/src/main/java/com/hdesrosiers/criminalintent02/CrimeListFragment.kt
+++ app/src/main/java/com/hdesrosiers/criminalintent02/CrimeListFragment.kt
@@ -10,6 +10,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -18,14 +19,9 @@ private const val TAG = "CrimeListFragment"
 class CrimeListFragment : Fragment() {
 
     private lateinit var crimeRecyclerView: RecyclerView
-    private var adapter: CrimeAdapter? = null
+    private var adapter: CrimeAdapter? = CrimeAdapter(emptyList())
 
     private val crimeListViewModel: CrimeListViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,14 +33,26 @@ class CrimeListFragment : Fragment() {
         crimeRecyclerView =
             view.findViewById(R.id.crime_recycler_view) as RecyclerView
         crimeRecyclerView.layoutManager = LinearLayoutManager(context)
-
-        updateUI()
+        crimeRecyclerView.adapter = adapter
 
         return view
     }
 
-    private fun updateUI() {
-        val crimes = crimeListViewModel.crimes
+    //observe the live data
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        crimeListViewModel.crimeListLiveData.observe(
+            viewLifecycleOwner,
+            Observer { crimes ->
+                crimes?.let {
+                    Log.i(TAG, "Got crimes ${crimes.size}")
+                    updateUI(crimes)
+                }
+            }
+        )
+    }
+
+    private fun updateUI(crimes: List<Crime>) {
         adapter = CrimeAdapter(crimes)
         crimeRecyclerView.adapter = adapter
     }
--- app/src/main/java/com/hdesrosiers/criminalintent02/CrimeListViewModel.kt
+++ app/src/main/java/com/hdesrosiers/criminalintent02/CrimeListViewModel.kt
@@ -3,18 +3,6 @@ package com.hdesrosiers.criminalintent02
 import androidx.lifecycle.ViewModel
 
 class CrimeListViewModel : ViewModel() {
-
-//    val crimes = mutableListOf<Crime>()
-//
-//    init {
-//        for (i in 0 until 100) {
-//            val crime = Crime()
-//            crime.title = "Crime #$i"
-//            crime.isSolved = i % 2 == 0
-//            crimes += crime
-//        }
-//    }
-
     private val crimeRepository = CrimeRepository.get()
-    val crimes = crimeRepository.getCrimes()
+    val crimeListLiveData = crimeRepository.getCrimes()
 }
